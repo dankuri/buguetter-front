fc8c5bfa02390ba2a939f23b528cdef1ee832882
samaxe13 <danek.il420@gmail.com>
2022-09-20 18:53:38 +0300

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0);
+    console.log(process.env.PORT);
+    return (
+        <div className="App">
+            <h1 className="text-left mr-40 inline-block font-bold text-green-500 cursor-default hover:drop-shadow-[0_0_20px_rgba(34,197,94,1)]">
+                buguetter
+            </h1>
+            <button
+                className="align-bottom"
+                onClick={() => setCount((count) => count + 1)}
+            >
+                count is {count}
+            </button>
+        </div>
+    );
+}
+
+export default App;
