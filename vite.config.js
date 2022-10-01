a5b9caeb16981e7f3a3e07dc32aa1203c87973b9
samaxe13 <danek.il420@gmail.com>
2022-10-01 16:21:30 +0300

    set ports & cors

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 8080,
+        cors: true,
+        host: 'localhost',
+    },
+    preview: {
+        port: 8081,
+        cors: true,
+        host: 'localhost',
+    },
 });
