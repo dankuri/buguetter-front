6b084db2622b62d90cd2535bc34b172412000e79
samaxe13 <danek.il420@gmail.com>
2022-10-01 16:24:11 +0300

    added diff title for dev build

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './styles/index.css';
 
+if (process.env.NODE_ENV === 'development') {
+    document.title = 'dev buguetter';
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
