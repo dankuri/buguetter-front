c22e1a6ea8a53220b1dbe3acc92d3dc85889692c
samaxe13 <danek.il420@gmail.com>
2022-10-01 17:55:15 +0300

    added loading state for server checking

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { getAccessToken } from '../accesToken';
 
 function Auth() {
@@ -8,37 +10,63 @@ function Auth() {
     //         setAccessToken(token);
     //     }
     // }
+    const api_url = import.meta.env.VITE_API_URL;
     const send_data = (ev) => {
         ev.preventDefault();
         let login = document.querySelector('#login').value;
         let password = document.querySelector('#pass').value;
-        console.log(`login: ${login}\npassword: ${password}`);
+        console.log(
+            `login: ${login}\npassword: ${password}\nsent to ${api_url}`
+        );
     };
 
+    const [isLoading, setLoading] = useState(true);
+
+    const checkServer = () => {
+        console.log('checking server');
+        fetch(`${api_url}/status`).then((response) => {
+            console.log(response['status']);
+            setLoading(false);
+        });
+    };
+
+    useEffect(checkServer, []);
+
     return (
-        <div
-            id="auth-form"
-            className="h-screen flex flex-col justify-center items-center"
-        >
-            <form action="#" onSubmit={send_data}>
-                <input
-                    type="login"
-                    className="block m-4 p-2 text-2xl rounded-lg"
-                    id="login"
-                    placeholder="login"
-                />
-                <input
-                    type="password"
-                    className="block m-4 p-2 text-2xl rounded-lg"
-                    name="pass"
-                    id="pass"
-                    placeholder="password"
-                />
-                <button type="submit" style={{ backgroundColor: '#1a1a1a' }}>
-                    login
-                </button>
-            </form>
-        </div>
+        <>
+            {isLoading === false ? (
+                <div
+                    id="auth-form"
+                    className="h-screen flex flex-col justify-center items-center"
+                >
+                    <form action="#" onSubmit={send_data}>
+                        <input
+                            type="login"
+                            className="block m-4 p-2 text-2xl rounded-lg"
+                            id="login"
+                            placeholder="login"
+                        />
+                        <input
+                            type="password"
+                            className="block m-4 p-2 text-2xl rounded-lg"
+                            name="pass"
+                            id="pass"
+                            placeholder="password"
+                        />
+                        <button
+                            type="submit"
+                            style={{ backgroundColor: '#1a1a1a' }}
+                        >
+                            login
+                        </button>
+                    </form>
+                </div>
+            ) : (
+                <div className="h-screen flex flex-col items-center justify-center text-2xl">
+                    i'm loading bruh
+                </div>
+            )}
+        </>
     );
 }
 
