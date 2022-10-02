cff8c1e1b5d25e0e815d94f67f867bc547e85434
samaxe13 <danek.il420@gmail.com>
2022-10-02 13:30:43 +0300

    added loading and error screens to the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,58 @@ import './App.css';
 import { getAccessToken } from '../accesToken.js';
 import Auth from './Auth';
 function App() {
+    const api_url = import.meta.env.VITE_API_URL;
+
+    const [isLoading, setLoading] = useState(true);
+    const [isFailed, setFailed] = useState(false);
+
+    function ErrorScreen() {
+        return (
+            <div className="h-screen flex flex-col items-center justify-center text-red-600 text-3xl">
+                server not working!
+            </div>
+        );
+    }
+
+    function LoadingScreen() {
+        return (
+            <div className="h-screen flex flex-col items-center justify-center text-3xl">
+                i'm loading bruh
+            </div>
+        );
+    }
+
+    async function checkServer() {
+        console.log('checking server');
+        await fetch(`${api_url}/status`)
+            .then((response) => {
+                if (response.ok) setLoading(false);
+            })
+            .catch((error) => {
+                console.error(error);
+                setFailed(true);
+            });
+    }
+
+    useEffect(() => {
+        checkServer();
+    }, []);
+
     return (
-        <div className="App">
-            <h1 className="fixed top-5 left-5 text-left w-6/12 inline-block font-bold text-green-500 cursor-default hover:drop-shadow-[0_0_20px_rgba(34,197,94,1)]">
-                buguetter
-            </h1>
-            <Auth />
-        </div>
+        <>
+            {isLoading === false ? (
+                <div className="App">
+                    <h1 className="fixed top-5 left-5 text-left w-6/12 inline-block font-bold text-green-500 cursor-default hover:drop-shadow-[0_0_20px_rgba(34,197,94,1)]">
+                        buguetter
+                    </h1>
+                    <Auth />
+                </div>
+            ) : isFailed === false ? (
+                <LoadingScreen />
+            ) : (
+                <ErrorScreen />
+            )}
+        </>
     );
 }
 
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,72 +1,50 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
 import { getAccessToken } from '../accesToken';
 
 function Auth() {
-    // if (getAccessToken() === '') {
-    //     let token = '';
-    //     token = prompt('no auth bruh', '');
-    //     if (token) {
-    //         setAccessToken(token);
-    //     }
-    // }
     const api_url = import.meta.env.VITE_API_URL;
+
     const send_data = (ev) => {
         ev.preventDefault();
         let login = document.querySelector('#login').value;
         let password = document.querySelector('#pass').value;
-        console.log(
-            `login: ${login}\npassword: ${password}\nsent to ${api_url}`
-        );
+        if (login === '') {
+            console.error('empty login!');
+        } else if (password === '') {
+            console.error('empty password!');
+        } else {
+            console.log(
+                `login: ${login}\npassword: ${password}\nsent to ${api_url}`
+            );
+        }
     };
-
-    const [isLoading, setLoading] = useState(true);
-
-    const checkServer = () => {
-        console.log('checking server');
-        fetch(`${api_url}/status`).then((response) => {
-            console.log(response['status']);
-            setLoading(false);
-        });
-    };
-
-    useEffect(checkServer, []);
 
     return (
-        <>
-            {isLoading === false ? (
-                <div
-                    id="auth-form"
-                    className="h-screen flex flex-col justify-center items-center"
-                >
-                    <form action="#" onSubmit={send_data}>
-                        <input
-                            type="login"
-                            className="block m-4 p-2 text-2xl rounded-lg"
-                            id="login"
-                            placeholder="login"
-                        />
-                        <input
-                            type="password"
-                            className="block m-4 p-2 text-2xl rounded-lg"
-                            name="pass"
-                            id="pass"
-                            placeholder="password"
-                        />
-                        <button
-                            type="submit"
-                            style={{ backgroundColor: '#1a1a1a' }}
-                        >
-                            login
-                        </button>
-                    </form>
-                </div>
-            ) : (
-                <div className="h-screen flex flex-col items-center justify-center text-2xl">
-                    i'm loading bruh
-                </div>
-            )}
-        </>
+        <div
+            id="auth-form"
+            className="h-screen flex flex-col justify-center items-center"
+        >
+            <form action="#" onSubmit={send_data}>
+                <input
+                    type="login"
+                    className="block m-4 p-2 text-2xl rounded-lg"
+                    id="login"
+                    placeholder="login"
+                />
+                <input
+                    type="password"
+                    className="block m-4 p-2 text-2xl rounded-lg"
+                    name="pass"
+                    id="pass"
+                    placeholder="password"
+                />
+                <a href="/register" className="bg-">
+                    register
+                </a>
+                <button type="submit" className="bg-slate-800">
+                    login
+                </button>
+            </form>
+        </div>
     );
 }
 
