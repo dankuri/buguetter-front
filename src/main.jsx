e410b8b2dc82febba69deef0e694a4d581b4aaa3
samaxe13 <danek.il420@gmail.com>
2022-10-04 21:44:46 +0300

    added Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './styles/index.css';
+import { BrowserRouter} from "react-router-dom"
 
 if (process.env.NODE_ENV === 'development') {
     document.title = 'dev buguetter';
@@ -9,6 +10,8 @@ if (process.env.NODE_ENV === 'development') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
