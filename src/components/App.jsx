7e8185d666e0b2d03b671f5b25b95a587ba190f6
samaxe13 <danek.il420@gmail.com>
2022-10-04 21:45:39 +0300

    extracted screens to files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import { getAccessToken, refreshAccessToken } from '../accesToken.js';
 import './App.css';
+
+import ErrorScreen from './ErrorScreen.jsx';
+import LoadingScreen from './LoadingScreen.jsx'; 
 import Auth from './Auth';
 import Navbar from './Navbar';
+
 export default function App() {
     const api_url = import.meta.env.VITE_API_URL;
 
@@ -11,26 +17,6 @@ export default function App() {
     const [userName, setUserName] = useState('');
 
     let isCheckingServer = false;
-
-    function ErrorScreen() {
-        return (
-            <div className="h-screen flex flex-col items-center justify-center text-red-600 text-3xl">
-                server not working!
-            </div>
-        );
-    }
-
-    function LoadingScreen() {
-        return (
-            <div className="h-screen flex flex-col items-center justify-center text-3xl">
-                i'm loading bruh
-            </div>
-        );
-    }
-
-    function recheck() {
-        if (!isCheckingServer) setTimeout(checkServer, 5000);
-    }
 
     async function getUser(token) {
         await fetch(`${api_url}/get_user_data`, {
@@ -45,6 +31,10 @@ export default function App() {
             .catch(error => console.log(error))
     }
 
+    function recheck() {
+            if (!isCheckingServer) setTimeout(checkServer, 5000);
+    }
+    
     async function checkServer() {
         isCheckingServer = true;
         await fetch(`${api_url}/status`)
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+    return (
+        <div className="h-screen flex flex-col items-center justify-center text-3xl">
+            i'm loading bruh
+        </div>
+    );
+}
