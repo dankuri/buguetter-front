7a13d21b886b29eb8592ef98fc58e896d71523f5
samaxe13 <danek.il420@gmail.com>
2022-10-04 21:02:38 +0300

    refresh access token in same file

--- src/accesToken.js
+++ src/accesToken.js
@@ -6,6 +6,30 @@ export const setAccessToken = (inputToken) => {
     accessToken = inputToken;
 };
 
+export const refreshAccessToken = async () => {
+    const api_url = import.meta.env.VITE_API_URL;
+    await fetch(`${api_url}/refresh`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${
+                document.cookie
+                    .split('; ')
+                    .find((row) => row.startsWith('refresh_token='))
+                    ?.split('=')[1]
+            }`,
+        },
+        body: JSON.stringify({}),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            setAccessToken(data['access_token']);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
 export const getAccessToken = () => {
     return accessToken;
 };
