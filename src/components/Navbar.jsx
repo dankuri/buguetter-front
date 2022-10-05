8e080015993924f376b75834efde9af04b80159e
samaxe13 <danek.il420@gmail.com>
2022-10-05 17:36:44 +0300

    logo is link to /

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
     return (
-        <h1 className="fixed top-5 left-5 text-left w-6/12 inline-block font-bold text-green-500 cursor-default hover:drop-shadow-[0_0_20px_rgba(34,197,94,1)]">
-            buguetter
-        </h1>
+        <Link to={"/"}>
+            <h1 className="fixed top-5 left-5 text-left w-6/12 inline-block font-bold text-green-500 cursor-default hover:drop-shadow-[0_0_20px_rgba(34,197,94,1)]">
+                buguetter
+            </h1>
+        </Link>
     );
 }
 
