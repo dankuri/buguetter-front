916af659dc73552469fd9f40df0f83cd7791dfa4
samaxe13 <danek.il420@gmail.com>
2022-10-05 15:01:51 +0300

    added proxy & env var for it

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 8080,
-        cors: true,
-        host: 'localhost',
-    },
-    preview: {
-        port: 8081,
-        cors: true,
-        host: 'localhost',
-    },
-});
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            port: 8080,
+            cors: true,
+            host: 'localhost',
+            proxy: {
+                '/api': {
+                    target: process.env.VITE_API_URL,
+                    changeOrigin: true,
+                    secure: false
+                }
+            }
+        },
+        preview: {
+            port: 8081,
+            cors: true,
+            host: 'localhost',
+        },
+    });
+}
