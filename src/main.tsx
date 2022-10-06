428a39f5ef53143aeb411a8ddfdd31eeb58306ce
samaxe13 <danek.il420@gmail.com>
2022-10-06 03:40:44 +0300

    added type for root el

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'development') {
     document.title = 'dev buguetter';
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
