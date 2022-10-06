a085fb956b9fc8981622086051ea020928557abf
samaxe13 <danek.il420@gmail.com>
2022-10-06 12:53:37 +0300

    corrected import & camelCased

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
-import { setAccessToken } from './accesToken';
+import { setAccessToken } from './accessToken';
 
-export const apiLogin = async (login_data: {
+export const apiLogin = async (loginData: {
     login: string;
     password: string;
 }) => {
@@ -9,17 +9,17 @@ export const apiLogin = async (login_data: {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(login_data)
+        body: JSON.stringify(loginData)
     });
     try {
         const data = await res.json();
         setAccessToken(data['token']);
     } catch (error) {
-        console.error(error);
+        throw error;
     }
 };
 
-export const apiRegister = async (register_data: {
+export const apiRegister = async (registerData: {
     name: string;
     login: string;
     password: string;
@@ -29,7 +29,7 @@ export const apiRegister = async (register_data: {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(register_data)
+        body: JSON.stringify(registerData)
     });
     try {
         const data = await res.json();
@@ -37,6 +37,6 @@ export const apiRegister = async (register_data: {
             throw data['response'];
         }
     } catch (error) {
-        console.error(error);
+        throw error;
     }
 };
