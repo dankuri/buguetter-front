6f85531e38a470b0b4ab59ce6d2ba82c9c422c6e
samaxe13 <danek.il420@gmail.com>
2022-10-06 03:40:33 +0300

    added try catch and type

--- src/accesToken.ts
+++ src/accesToken.ts
@@ -2,26 +2,25 @@
 
 let accessToken = '';
 
-export const setAccessToken = (inputToken) => {
+export const setAccessToken = (inputToken: string) => {
     accessToken = inputToken;
 };
 
 export const refreshAccessToken = async () => {
-    await fetch(`/api/refresh`, {
+    const res = await fetch(`/api/refresh`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
         credentials: 'include'
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            setAccessToken(data['access_token']);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-}
+    });
+    try {
+        const data = await res.json();
+        setAccessToken(data['access_token']);
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 export const getAccessToken = () => {
     return accessToken;
