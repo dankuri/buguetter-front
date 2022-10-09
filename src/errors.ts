708b599dcd6b3eec83bc91552d86bf5c4f2d9afa
samaxe13 <danek.il420@gmail.com>
2022-10-09 22:01:44 +0200

    added error list

---
+++ src/errors.ts
@@ -0,0 +1,12 @@
+export const errors: { [key: number]: string } = {
+    1: 'no login data in request',
+    2: 'no user with this login',
+    3: 'wrong password',
+    4: 'this login is already taken',
+    5: 'this name is already taken',
+    6: 'db connection error',
+    7: 'no text in post',
+    8: 'no refresh and/or access token',
+    9: 'token modified elsewhere',
+    10: 'token revoked'
+};
