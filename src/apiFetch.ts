08020df7b51e1561b3a40ffbedb5e7e9a6b7eb3c
samaxe13 <danek.il420@gmail.com>
2022-10-12 13:17:59 +0200

    renamed for new fetching func

--- src/apiFetch.ts
+++ src/apiFetch.ts
@@ -1,18 +1,23 @@
-import { statusErrorHandler } from './errors';
+import { statusErrorHandler } from './errors'
 
-export const apiFetchCookie = async (url: string, method: string) => {
+export const apiFetchSendCookie = async (url: string, method: string) => {
     const res = await fetch(url, {
         method: method,
         headers: {
             'Content-Type': 'application/json'
         },
         credentials: 'include'
-    });
-    const { msg, error }: ApiResponse = await res.json();
-    return statusErrorHandler({ msg, error });
-};
+    })
+    const { msg, error }: ApiStatusResponse = await res.json()
+    // TODO: remove this log when tested
+    if (msg == 'new_token') console.log('refreshed token')
+    msg == 'new_token'
+        ? apiFetchSendCookie(url, method)
+        : statusErrorHandler({ msg, error })
+    return statusErrorHandler({ msg, error })
+}
 
-export const apiFetchData = async (
+export const apiFetchSendData = async (
     url: string,
     method: string,
     data: { [key: string]: string }
@@ -24,7 +29,40 @@ export const apiFetchData = async (
         },
         credentials: 'include',
         body: JSON.stringify(data)
-    });
-    const { msg, error }: ApiResponse = await res.json();
-    return statusErrorHandler({ msg, error });
-};
+    })
+    const { msg, error }: ApiStatusResponse = await res.json()
+    // TODO: remove this log when tested
+    if (msg == 'new_token') console.log('refreshed token')
+    msg == 'new_token'
+        ? apiFetchSendCookie(url, method)
+        : statusErrorHandler({ msg, error })
+    return statusErrorHandler({ msg, error })
+}
+// FIXME: not working properly, returns invalid response
+// export const apiFetchGetData = async (
+//     url: string,
+//     method: string,
+//     data?: { [key: string]: string }
+// ) => {
+//     const res = await fetch(url, {
+//         method: method,
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         credentials: 'include',
+//         body: JSON.stringify(data)
+//     })
+//     let returnData = await res.json()
+//     if (returnData.msg == 'new_token') {
+//         console.log('refreshed token')
+//         returnData = apiFetchGetData(url, method, data)
+//     }
+//     if (returnData.error != 0) {
+//         const { msg, error }: ApiStatusResponse = returnData
+//         console.log('errored' + error)
+//         console.log(`send: ${method} ${url} ${data}`)
+//         console.log(`got: ${JSON.stringify(returnData)}`)
+//         return statusErrorHandler({ msg, error })
+//     }
+//     return returnData
+// }
