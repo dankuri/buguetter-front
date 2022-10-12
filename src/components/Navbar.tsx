fbe6a27443845b307f0998973436efb1520b5ed6
samaxe13 <danek.il420@gmail.com>
2022-10-12 13:51:47 +0200

    adjusted navbar and logoutBtn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,36 @@
-import { Link } from 'react-router-dom';
-import LogoutBtn from './LogoutBtn';
+import { Link } from 'react-router-dom'
+import LogoutBtn from './LogoutBtn'
 
 type Props = {
-    isLoggedIn: boolean;
-    setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
-    setUserName: React.Dispatch<React.SetStateAction<string>>;
-};
+    isLoggedIn: boolean
+    userName: string
+    setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
+    setUserName: React.Dispatch<React.SetStateAction<string>>
+}
 
-function Navbar({ isLoggedIn, setLoggedIn, setUserName }: Props) {
+function Navbar({ isLoggedIn, userName, setLoggedIn, setUserName }: Props) {
     return (
-        <>
+        <div className="sticky flex top-0 w-screen border-b-2 py-2 px-6 bg-slate-700 justify-between items-center">
             <Link to={'/'}>
-                <h1 className="fixed top-5 left-5 text-left w-6/12 inline-block font-bold text-green-500 cursor-default hover:drop-shadow-[0_0_20px_rgba(34,197,94,1)]">
+                <h1 className="text-left inline-block font-bold text-green-500 cursor-default hover:drop-shadow-[0_0_20px_rgba(34,197,94,1)]">
                     buguetter
                 </h1>
             </Link>
             {isLoggedIn && (
-                <LogoutBtn
-                    setUserName={setUserName}
-                    setLoggedIn={setLoggedIn}
-                />
+                <div className="flex items-center">
+                    <img
+                        src={`https://joeschmoe.io/api/v1/${userName}`}
+                        alt="ur profile pic"
+                        className="w-16 h-16 mr-4 rounded-full bg-gray-800"
+                    />
+                    <LogoutBtn
+                        setUserName={setUserName}
+                        setLoggedIn={setLoggedIn}
+                    />
+                </div>
             )}
-        </>
-    );
+        </div>
+    )
 }
 
-export default Navbar;
+export default Navbar
