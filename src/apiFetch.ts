28e032c2aedb753bbb1aa707b6bcc926b74b68d8
samaxe13 <danek.il420@gmail.com>
2022-10-14 00:24:57 +0200

    added get data wrapper

--- src/apiFetch.ts
+++ src/apiFetch.ts
@@ -12,7 +12,7 @@ export const apiFetchSendCookie = async (url: string, method: string) => {
     // TODO: remove this log when tested
     if (msg == 'new_token') console.log('refreshed token')
     msg == 'new_token'
-        ? await apiFetchSendCookie(url, method)
+        ? apiFetchSendCookie(url, method)
         : statusErrorHandler({ msg, error })
     return statusErrorHandler({ msg, error })
 }
@@ -36,6 +36,27 @@ export const apiFetchSendData = async (
     msg == 'new_token'
         ? apiFetchSendCookie(url, method)
         : statusErrorHandler({ msg, error })
+    return statusErrorHandler({ msg, error })
+}
+
+export const apiFetchGetData = async (
+    url: string,
+    method: string,
+    data?: { [key: string]: number | string }
+): Promise<{ [key: string]: number | string }> => {
+    const res = await fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify(data)
+    })
+    const returnData = await res.json()
+    if (returnData.msg == 'new_token') {
+        return apiFetchGetData(url, method, data)
+    } else if (!returnData.error) return returnData
+    const { msg, error } = returnData
     return statusErrorHandler({ msg, error })
 }
 // FIXME: not working properly, returns invalid response
--- src/getUser.ts
+++ src/getUser.ts
@@ -1,31 +1,33 @@
+import { apiFetchGetData } from './apiFetch'
 import { catchNull } from './catchNull'
 
 const getUser = async () => {
-    console.log('start get user')
-    const res = await fetch(`/api/get_user_data`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        credentials: 'same-origin'
-    })
-    console.log(res)
-    const data = await res.json()
-    console.log(data)
-    // TODO: extract this kind of fetch in apiFetch
-    if (data.msg == 'new_token') {
-        console.log('refreshed token in getUser')
-        const newRes = await fetch(`/api/get_user_data`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'same-origin'
-        })
-        const newData = await newRes.json()
-        return newData
-    } else if (!data.error) return data
-    else return false
+    // console.log('start get user')
+    // const res = await fetch(`/api/get_user_data`, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     credentials: 'same-origin'
+    // })
+    // console.log(res)
+    // const data = await res.json()
+    // console.log(data)
+    // // TODO: extract this kind of fetch to apiFetch
+    // if (data.msg == 'new_token') {
+    //     console.log('refreshed token in getUser')
+    //     const newRes = await fetch(`/api/get_user_data`, {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         credentials: 'same-origin'
+    //     })
+    //     const newData = await newRes.json()
+    //     return newData
+    // } else if (!data.error) return data
+    // else return false
+    return apiFetchGetData('/api/get_user_data', 'GET')
 }
 
 export const getUserCatching = catchNull(getUser)
