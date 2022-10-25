25f74aaabe6febdb60a66f8be20541a24f32431a
samaxe13 <danek.il420@gmail.com>
2022-10-25 13:09:25 +0200

    add graphql logic to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,95 +1,103 @@
 import { useEffect, useState } from 'react'
-import { Routes, Route, useNavigate, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 
-import useServerCheck from '../hooks/useServerCheck.jsx'
-import { getUserCatching } from '../getUser.js'
-
-import ErrorScreen from './ErrorScreen.jsx'
-import LoadingScreen from './LoadingScreen.jsx'
-import LoginForm from './LoginForm.jsx'
-import RegisterForm from './RegisterForm.jsx'
-import Navbar from './Navbar.jsx'
-import Profile from './Profile.jsx'
-
-type UserData = {
-    name: string
-    user_id: number
-    follow: number
-    following: number
-}
+import LoadingScreen from './LoadingScreen'
+import LoginForm from './LoginForm'
+import RegisterForm from './RegisterForm'
+import Navbar from './Navbar'
+import Profile from './Profile'
+import { useQuery } from '@apollo/client'
+import { UserFullDocument } from '../graphql/gql'
 
 export default function App() {
-    const [isLoading, setLoading] = useState(true)
-    const [isLoggedIn, setLoggedIn] = useState(false)
-    const [userName, setUserName] = useState('')
-    const [userId, setUserId] = useState(-1)
-    const [followers, setFollowers] = useState(0)
-    const [following, setFollowing] = useState(0)
-    const isFailed = useServerCheck()
     const navigate = useNavigate()
-    // FIXME: fires 2 times instead 1 on prod (3 on dev even)
-    const asyncEffect = async () => {
-        if (!isFailed) {
-            const userData: UserData = await getUserCatching()
-            console.log(userData)
-            if (userData['name']) {
-                setUserName(userData['name'])
-                setUserId(userData['user_id'])
-                setFollowers(userData['follow'])
-                setFollowing(userData['following'])
-                setLoggedIn(true)
-            } else {
-                navigate('/login')
-            }
-            setLoading(false)
-        }
+    const [isLoggedIn, setLoggedIn] = useState(false)
+    const { data, loading, error, refetch } = useQuery(UserFullDocument, {
+        variables: { userId: 0 },
+        fetchPolicy: 'no-cache'
+    })
+
+    useEffect(() => {
+        if (data) {
+            setLoggedIn(true)
+            navigate('/')
+        } else if (error) navigate('/login')
+    }, [data, loading, isLoggedIn])
+
+    if (loading) return <LoadingScreen />
+
+    if (!data || error) {
+        return (
+            <div className="flex min-h-screen flex-col">
+                <Navbar
+                    userName=""
+                    isLoggedIn={false}
+                    setLoggedIn={setLoggedIn}
+                    refetch={refetch}
+                />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <h2 className="flex grow flex-col items-center justify-center text-3xl">
+                                    ur not logged in, go to
+                                    <Link to={'/login'} className={'btn'}>
+                                        login
+                                    </Link>
+                                </h2>
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <LoginForm
+                                setLoggedIn={setLoggedIn}
+                                refetch={refetch}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/register"
+                        element={
+                            <RegisterForm
+                                setLoggedIn={setLoggedIn}
+                                refetch={refetch}
+                            />
+                        }
+                    />
+                </Routes>
+            </div>
+        )
     }
 
-    useEffect(() => void asyncEffect(), [isLoggedIn, isFailed])
+    const userData = {
+        name: data.user.name,
+        userId: data.user.id
+    }
 
     return (
-        <div className="App flex min-h-screen flex-col">
-            {!isLoading ? (
+        <main className="flex min-h-screen flex-col">
+            {!loading ? (
                 <>
                     <Navbar
-                        userName={userName}
-                        setUserName={setUserName}
+                        userName={userData.name}
                         isLoggedIn={isLoggedIn}
                         setLoggedIn={setLoggedIn}
+                        refetch={refetch}
                     />
                     <Routes>
                         <Route
                             path="/"
-                            element={
-                                isLoggedIn ? (
-                                    <Profile
-                                        current={true}
-                                        name={userName}
-                                        followers={followers}
-                                        following={following}
-                                        userId={userId}
-                                    />
-                                ) : (
-                                    <>
-                                        <h2 className="flex grow flex-col items-center justify-center text-3xl">
-                                            ur not logged in, go to
-                                            <Link
-                                                to={'/login'}
-                                                className={'btn'}
-                                            >
-                                                login
-                                            </Link>
-                                        </h2>
-                                    </>
-                                )
-                            }
+                            element={<Profile current={true} {...userData} />}
                         />
                         <Route
                             path="/login"
                             element={
                                 <LoginForm
-                                    setUserName={setUserName}
                                     setLoggedIn={setLoggedIn}
+                                    refetch={refetch}
                                 />
                             }
                         />
@@ -97,18 +105,16 @@ export default function App() {
                             path="/register"
                             element={
                                 <RegisterForm
-                                    setUserName={setUserName}
                                     setLoggedIn={setLoggedIn}
+                                    refetch={refetch}
                                 />
                             }
                         />
                     </Routes>
                 </>
-            ) : !isFailed ? (
-                <LoadingScreen />
             ) : (
-                <ErrorScreen />
+                <LoadingScreen />
             )}
-        </div>
+        </main>
     )
 }
