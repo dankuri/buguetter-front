d40868f1011b93198a657ec8654b21e6ecb9c20f
dankuri <71091658+dankuri@users.noreply.github.com>
2024-01-23 20:50:01 +0100

    change vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-
     return defineConfig({
         plugins: [react()],
+        base: '/buguetter-front/',
         server: {
             port: 8080,
             cors: true,
